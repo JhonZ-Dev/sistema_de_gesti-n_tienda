76a3c5e5aafb4d0222aa681aae91746cdf9a6fc9
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-04 21:17:20 -0500

    Update tienda_virtual.py

--- tienda_virtual.py
+++ tienda_virtual.py
@@ -41,6 +41,17 @@ class Store:
         """Método que permite revisar el inventario de la tienda."""
         return [f"{product.name} - Stock: {product.stock}" for product in self.products]
 
+if __name__ == "__main__":
+    store = Store()
+
+    while True:
+        print("Options:")
+        print("1. Add a product")
+        print("2. List products")
+        print("3. Purchase a product")
+        print("4. Check inventory")
+        print("5. Quit")
+
         
     
 

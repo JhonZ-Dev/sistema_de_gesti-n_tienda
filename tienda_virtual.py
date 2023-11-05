bdc547bf66abc75cf0b31ccc38f7fbc75d59fdc0
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-04 21:16:13 -0500

    Update tienda_virtual.py

--- tienda_virtual.py
+++ tienda_virtual.py
@@ -14,8 +14,17 @@ class Store:
         self.purchase_history = []
         
     def add_product(self, name, price, stock):
+        """Método que permite agregar un producto a la tienda."""
         product = Product(name, price, stock)
         self.products.append(product)
+        
     def list_products(self):
+        """Método que permite listar los productos de la tienda."""
         return [f"{product.name} - Price: ${product.price} - Stock: {product.stock}" for product in self.products]
+    
+    
+    def purchase_product(self, name, quantity):
+        """Método que permite comprar un producto de la tienda."""
+        product = next((p for p in self.products if p.name.lower() == name.lower()), None)
+
 

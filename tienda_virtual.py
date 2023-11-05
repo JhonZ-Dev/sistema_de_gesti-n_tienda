969aff851cc64444094dca5a4fd92ee3d9914a05
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-04 21:14:08 -0500

    Update tienda_virtual.py

--- tienda_virtual.py
+++ tienda_virtual.py
@@ -1,3 +1,6 @@
 class Product:
     """Clase que representa un producto en general."""
-    
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock

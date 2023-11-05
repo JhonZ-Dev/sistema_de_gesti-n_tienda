3f861edda82e5f83fd0a21b60af5098e8de29b8d
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-04 21:14:17 -0500

    Update tienda_virtual.py

--- tienda_virtual.py
+++ tienda_virtual.py
@@ -1,6 +1,7 @@
 class Product:
     """Clase que representa un producto en general."""
     def __init__(self, name, price, stock):
+        """Constructor de la clase Product."""
         self.name = name
         self.price = price
         self.stock = stock
